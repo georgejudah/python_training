6055c0dda2f06d010dd089786cc8491e803cb6d7
Judah George <georgejudah5@gmail.com>
2023-10-20 01:39:34 +0530

    completed Lesson 2

---
+++ lists/app.py
@@ -0,0 +1,9 @@
+#initialize a list
+a = ['sarah', 'test', 'jude', 'sian']
+#index
+# print(a[0])
+
+# 'sarah' + '_chennai' = sarah_chennai
+#for loop
+for each_item in a:
+    print(each_item + '_chennai')
