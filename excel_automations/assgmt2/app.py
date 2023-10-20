e0fda9054cc178daf6344e89fc999c6c06ab8be5
Judah George <georgejudah5@gmail.com>
2023-10-21 00:25:45 +0530

    completed assgm1 and updated assgnm2 questions

---
+++ excel_automations/assgmt2/app.py
@@ -0,0 +1,11 @@
+# In the AmazonBooks dataframe
+# the AmazonBooks excel file which was shared,
+# 1. find the number of rows containing null values in 1.)User Rating 2.)Reviews
+# 2. If you find any null values, remove that particular rows out of the dataframe and print the number of rows available after deleting
+# BONUS questions
+# **3. Find the number of books having rating more than 4.5 and print out the names of each books
+# **4. Find the number of books having books having 'Non Fiction' genre
+
+# ASSIGNMENT #2
+# 1. remove all rows having duplicate names
+# 2. Save this dataframe into an excel file(along all of the steps which we did earlier as well), don't need a column for index
